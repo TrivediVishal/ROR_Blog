a083e95072140eebb35b40c13fd96c34c7adc687
TrivediVishal <montutrivedi10@gmail.com>
2024-03-27 11:06:25 +0530

    Next up 8.4 Generating a Controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get "/articles", to: "articles#index"
   # get "articles/:id", to: "articles#show"
   #The above two lines are replaced by the following line
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
